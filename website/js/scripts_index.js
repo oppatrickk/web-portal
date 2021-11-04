aa64b1e9e8e7f85b8134646f77a65e71bafceec9
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2021-11-05 02:21:05 +0800

    Version 0.6
    Login

--- website/js/scripts_index.js
+++ website/js/scripts_index.js
@@ -7,6 +7,27 @@
 // Scripts
 //
 
+function validation()
+{
+    var id=document.f1.user.value;
+    var ps=document.f1.pass.value;
+    if(id.length=="" && ps.length=="") {
+        alert("User Name and Password fields are empty");
+        return false;
+    }
+    else
+    {
+        if(id.length=="") {
+            alert("User Name is empty");
+            return false;
+        }
+        if (ps.length=="") {
+            alert("Password field is empty");
+            return false;
+        }
+    }
+}
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
