3de7b36d21643c523dc44ceef9bfc0ca2b427897
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2021-11-09 03:26:12 +0800

    Version 0.8

--- website/authentication.php
+++ website/authentication.php
@@ -14,10 +14,12 @@ $result = mysqli_query($con, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $count = mysqli_num_rows($result);
 
-if($count == 1){
-    echo "<h1><center> Login successful </center></h1>";
-}
-else{
+if($count == 1) {
+    //session_register("myusername");
+    //$_SESSION['login_user'] = $myusername;
+
+    header("location: dashboard.html");
+}else{
     echo "<h1> Login failed. Invalid username or password.</h1>";
 }
 ?>
