533a21eae825dc111a4d4bb11600f001091002ec
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2021-12-08 21:46:21 +0800

    Version 2.1

--- pages/dashboard/dashboard.php
+++ pages/dashboard/dashboard.php
@@ -8,7 +8,7 @@ include '../../database/config.php';
 
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: ../login/login.php");
+    header("location: ../../index");
     exit;
 }
 
