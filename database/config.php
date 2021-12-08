7277c6c84666a770f1c5f2b3da657eb534854925
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2021-12-08 22:21:03 +0800

    Version 2.1

--- database/config.php
+++ database/config.php
@@ -3,9 +3,11 @@ const DB_SERVER = '34.124.248.106';
 const DB_USERNAME = 'root';
 const DB_PASSWORD = '123456';
 const DB_NAME = 'codex';
+const DB_INSTANCE = '/cloudsql/codex-bu:asia-southeast1:codex';
 
 /* Attempt to connect to MySQL database */
-$link = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = new mysqli(null, DB_USERNAME, DB_PASSWORD, DB_NAME, null, DB_INSTANCE);
+
 
 // Check connection
 if ($link->connect_error) {
