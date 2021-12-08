d442685957896063d44a9996c9355f73bf46cda6
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2021-12-08 22:25:15 +0800

    Update config.php

--- database/config.php
+++ database/config.php
@@ -3,11 +3,12 @@ const DB_SERVER = '34.124.248.106';
 const DB_USERNAME = 'root';
 const DB_PASSWORD = '123456';
 const DB_NAME = 'codex';
-const DB_INSTANCE = '/cloudsql/codex-bu:asia-southeast1:codex';
+const DB_INSTANCE = ':/cloudsql/codex-bu:asia-southeast1:codex';
 
 /* Attempt to connect to MySQL database */
-$link = new mysqli(null, DB_USERNAME, DB_PASSWORD, DB_NAME, null, DB_INSTANCE);
+$link = mysql_connect(DB_INSTANCE, DB_USERNAME, DB_PASSWORD);
 
+mysql_select_db(DB_NAME);
 
 // Check connection
 if ($link->connect_error) {
