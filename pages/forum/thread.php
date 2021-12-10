67a8d6935e6c9d36cd802d91516e15fffcfec45b
Terentado <91873666+Terentado@users.noreply.github.com>
2021-12-10 20:43:52 +0800

    Update thread.php

--- pages/forum/thread.php
+++ pages/forum/thread.php
@@ -74,6 +74,16 @@ require_once "../../database/config.php";
   }
 
   ?>
+  
+  <?php
+    $method = $_SERVER['REQUEST_METHOD'];
+    if($method=='POST'){
+      //Insert the comment into the database
+      $comment = $_POST['comment'];
+      $sql = "INSERT INTO `comments` (`comment_content`, `thread_id`, `comment_by`, `comment_time`) VALUES ('$comment', '$id', '0',current_timestamp()) ";
+      $result = mysqli_query($conn, $sql);
+    }
+  ?>
 
   <div class="container my-4">
     <div class="jumbotron">
@@ -87,25 +97,47 @@ require_once "../../database/config.php";
     </div>
   </div>
 
+  <?php 
+    if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){ 
+    echo '<div class="container">
+        <h1 class="py-2">Post a Comment</h1> 
+        <form action= "'. $_SERVER['REQUEST_URI'] . '" method="post"> 
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Type your comment</label>
+                <textarea class="form-control" id="comment" name="comment" rows="3"></textarea>
+                <input type="hidden" name="sno" value="'. $_SESSION["sno"]. '">
+            </div>
+            <button type="submit" class="btn btn-success">Post Comment</button>
+        </form> 
+    </div>';
+    }
+    else{
+        echo '   
+        <div class="container">
+        <h1 class="py-2">Post a Comment</h1> 
+           <p class="lead">You are not logged in. Please login to be able to post comments.</p>
+        </div>
+        ';
+    }
+    ?>
+
+
+
   <div class="container">
-
-    <h1 class="py-2">Discussion</h1>
-
-    <!-- <?php
-          $id = $_GET['catid'];
-          $sql = "SELECT * FROM `threads` WHERE thread_cat_id=$id";
-          $result = mysqli_query($link, $sql);
+   <?php
+          $id = $_GET['threadid'];
+          $sql = "SELECT * FROM `comments` WHERE thread_id=$id";
+          $result = mysqli_query($conn, $sql);
           while ($row = mysqli_fetch_assoc($result)) {
-            $id = $row['thread_id'];
-            $title = $row['thread_title'];
-            $desc = $row['thread_description'];
-
+            $id = $row['comment_id'];
+            $content = $row['comment_content'];
+            $comment_time = $row['comment_time'];
 
             echo '<div class="media my-3">
       <img class="mr-3" src="assets/Default User.jpg" width="40px" alt="Generic placeholder image">
       <div class="media-body">
-        <h5 class="mt-0"><a class="text-dark" href="thread.php?threadid='  . $id  . '">' . $title . '</a></h5>
-        ' . $desc . '
+      <p class="font-weight-bold my-0"><b> Asked by a Anonymous User at ' .$comment_time. '</b></p>
+        ' . $content . '
       </div>
     </div>';
           }
@@ -114,22 +146,14 @@ require_once "../../database/config.php";
           if ($noResult) {
             echo '<div class="jumbotron jumbotron-fluid">
           <div class="container">
-              <p class="lead">No Threads Found</p>
+              <p class="lead">No Comments Found</p>
               <p class="lead"> Be the first person to ask a question</p>
           </div>
        </div> ';
           }
-
-
-          ?>-->
-
-
+          ?>
 
     <!-- remove later; putting this just to check html allignment-->
-
-
-
-
 
   </div>
 
