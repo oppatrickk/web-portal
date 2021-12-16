aa498030c85ec3138828642a50e26b5a4176fea0
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2021-12-16 19:01:55 +0800

    test

--- pages/login/email.php
+++ pages/login/email.php
@@ -1,49 +1,69 @@
-"<!DOCTYPE html>
-<html lang="en">
+'<!DOCTYPE html>
+<html lang='en'>
 
 <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
 
     <!-- Title -->
     <title>codeX</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+    <link rel='icon' type='image/x-icon' href='assets/favicon.ico'>
 
     <!-- Bootstrap CSS and Icons -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet">
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css' rel='stylesheet'>
 
     <!-- External CSS -->
-    <link href="css/index.css" rel="stylesheet" />
+    <link href='css/index.css' rel='stylesheet' />
 
     <!-- Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel='preconnect' href='https://fonts.googleapis.com'>
+    <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
     <link href='https://fonts.googleapis.com/css?family=Noto Sans' rel='stylesheet'>
 
 </head>
 
 <body style='height: 100%; padding: 50px; background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);'>
 
-<div class='container h-100 d-flex justify-content-center'>
-    <div class='jumbotron my-auto col-5' style='background-color: white; border-radius: 16px; padding: 25px;'>
+<div>
+    <div style='background-color: white; border-radius: 16px; padding: 25px; width: 30%; margin: auto;'>
         <center>
-            <div class='d-flex justify-content-center'>
-                <img src='https://i.ibb.co/qjwp8ZK/logo2.png' style='width: 6rem; height: 2rem;'>
+            <div>
+                <a href='https://codex-bu.appspot.com/'>
+                    <img src='https://i.ibb.co/VTB4vfy/logo1.png' style='width: 6rem; height: 6rem;'>
+                </a>
             </div>
         </center>
-        <h4 class='fw-bold mt-5 text-primary'>Forgot your Password?</h4>
-        <p>Hello, we have received a request to reset your password.</p>
-        <br>
-        <p>Use the following password to log in to your account:</p>
-        <center>
-            <div class='border border-primary text-black-50 fw-bold p-2 col-4' style='border-radius: 32px; border: 2px #000dff;'>test</div>
-        </center>
+        <div style='padding-left: 15px; padding-right: 15px;'>
+            <br>
+            <h1 style='font-weight: bold; color: royalblue; margin-bottom: 12px;'>Forgot your Password?</h1>
+            <p>Hello, we have received a request to reset your password.</p>
+            <br>
+            <p>Use the following password to log in to your account:</p>
+            <center>
+                <div style='border-radius: 32px; border: 1px solid #000dff; color: black; padding: 8px; width: 50%; font-weight: bold'>test</div>
+            </center>
+            <br>
+            <p>We recommend changing your password after signing in.</p>
+            <br>
+            <p>Having trouble? <a href='https://codex-bu.appspot.com/pages/footer/report.php' style='text-decoration: none;'>Contact us</a></p>
+            <p>Didn't request a password reset? You can ignore this message.</p>
+            <br>
+        </div>
     </div>
+    <center>
+        <br>
+        <a href='https://codex-bu.appspot.com/'>
+            <img src='https://i.ibb.co/pZ0Lmqn/logo2-white.png' style='width: 7rem; height: 2.5rem;'>
+        </a>
+        <br><br>
+        <p style='color:white; font-size: 12px; opacity: 75%'>This password will expire in the next 24 hours.</p>
+        <p style='color:white; font-size: 12px; opacity: 100%'>codeX © 2021</p>
+    </center>
 </div>
 
 </body>
--- pages/login/recovery.php
+++ pages/login/recovery.php
@@ -69,23 +69,44 @@ if(isset($_REQUEST['btn_recover'])){
 
 
 
-                    $message_body = "<body style='height: 100%; padding: 50px; background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);'>
+                    $message_body = "
+<body style='height: 100%; padding: 50px; background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);'>
 
-<div class='container h-100 d-flex justify-content-center'>
-    <div class='jumbotron my-auto col-5' style='background-color: white; border-radius: 16px; padding: 25px;'>
+<div>
+    <div style='background-color: white; border-radius: 16px; padding: 25px; width: 30%; margin: auto;'>
         <center>
-            <div class='d-flex justify-content-center'>
-                <img src='https://i.ibb.co/qjwp8ZK/logo2.png' style='width: 6rem; height: 2rem;'>
+            <div>
+                <a href='https://codex-bu.appspot.com/'>
+                    <img src='https://i.ibb.co/VTB4vfy/logo1.png' style='width: 6rem; height: 6rem;'>
+                </a>
             </div>
         </center>
-        <h4 class='fw-bold mt-5 text-primary'>Forgot your Password?</h4>
-        <p>Hello, we have received a request to reset your password.</p>
-        <br>
-        <p>Use the following password to log in to your account:</p>
-        <center>
-            <div class='border border-primary text-black-50 fw-bold p-2 col-4' style='border-radius: 32px; border: 2px #000dff;'>test</div>
-        </center>
+        <div style='padding-left: 15px; padding-right: 15px;'>
+            <br>
+            <h1 style='font-weight: bold; color: royalblue; margin-bottom: 12px;'>Forgot your Password?</h1>
+            <p>Hello <?php echo $first_name . " ". $last_name; ?> , we have received a request to reset your password.</p>
+            <br>
+            <p>Use the following password to log in to your account:</p>
+            <center>
+                <div style='border-radius: 32px; border: 1px solid #000dff; color: black; padding: 8px; width: 50%; font-weight: bold'><?php echo $recovery_password; ?></div>
+            </center>
+            <br>
+            <p>We recommend changing your password after signing in.</p>
+            <br>
+            <p>Having trouble? <a href='https://codex-bu.appspot.com/pages/footer/report.php' style='text-decoration: none;'>Contact us</a></p>
+            <p>Didn't request a password reset? You can ignore this message.</p>
+            <br>
+        </div>
     </div>
+    <center>
+        <br>
+        <a href='https://codex-bu.appspot.com/'>
+            <img src='https://i.ibb.co/pZ0Lmqn/logo2-white.png' style='width: 7rem; height: 2.5rem;'>
+        </a>
+        <br><br>
+        <p style='color:white; font-size: 12px; opacity: 75%'>This password will expire in the next 24 hours.</p>
+        <p style='color:white; font-size: 12px; opacity: 100%'>codeX © 2021</p>
+    </center>
 </div>
 
 </body>";
