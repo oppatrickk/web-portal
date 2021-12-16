adab801b9b0afe007328f2b3a26596780ee299d4
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2021-12-16 20:24:09 +0800

    Ts

--- pages/login/email.php
+++ pages/login/email.php
@@ -27,49 +27,45 @@
 
 </head>
 
-<body style='height: 100%; padding: 2%; background-color: royalblue'>
+<body style='height: 100%; padding: 2%; background-color: royalblue;'>
 
 <div>
-    <div style='background-color: white; border-radius: 16px; padding: 20px; width: 550px; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;'>
+    <div style='background-color: white; border-radius: 16px; padding: 25px; width: 600px; margin: auto; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;'>
         <center>
             <div>
                 <a href='https://codex-bu.appspot.com/'>
-                    <img src='https://i.ibb.co/VTB4vfy/logo1.png' style='width: 6rem; height: 6rem;'>
+                    <img src='https://i.ibb.co/qjwp8ZK/logo2.png' style='width: 7rem; height: 2.5rem;'>
                 </a>
             </div>
         </center>
         <div style='padding-left: 15px; padding-right: 15px;'>
             <br>
             <h1 style='font-weight: bold; color: royalblue; margin-bottom: 12px;'>Forgot your Password?</h1>
-            <p>Hello, we have received a request to reset your password.</p>
+            <p style='color:black'>Hello <b>" .  $first_name . " " . $last_name . "</b>, we have received a request to reset your password.</p>
             <br>
-            <p>Use the following password to log in to your account:</p>
+            <p style='color:black'>Use the following password to log in to your account:</p>
             <center>
-                <div style='border-radius: 32px; border: 1px solid #000dff; color: black; padding: 8px; width: 50%; font-weight: bold'>test</div>
+                <div style='border-radius: 32px; border: 1px solid #000dff; color: black; padding: 8px; width: 50%; font-weight: bold'>" .$recovery_password. "</div>
             </center>
             <br>
-            <p>We recommend changing your password after signing in.</p>
+            <p style='color:black'>We recommend changing your password after signing in.</p>
             <br>
-            <p>Having trouble? <a href='https://codex-bu.appspot.com/pages/footer/report.php' style='text-decoration: none;'>Contact us</a></p>
-            <p>Didn't request a password reset? You can ignore this message.</p>
+            <p style='color:black'>Having trouble? <a href='https://codex-bu.appspot.com/pages/footer/report.php' style='text-decoration: none; color:blue'>Contact us</a></p>
+            <p style='color:black'>Didn't request a password reset? You can ignore this message.</p>
             <br>
         </div>
     </div>
-    <div style='width: 30%;'>
-    <center>
-        <br>
-        <p style='color:white; font-size: 12px; opacity: 75%'>This password will expire in the next 24 hours.</p>
-        <br>
-        <a href='https://codex-bu.appspot.com/'>
-            <img src='https://i.ibb.co/pZ0Lmqn/logo2-white.png' style='width: 7rem; height: 2.5rem;'>
-        </a>
-        <p style='color:white; font-size: 12px; opacity: 100%'>codeX © 2021</p>
-        <br>
-
-
-    </center>
+    <div style='width: 600px; margin: auto;'>
+        <center>
+            <br>
+            <p style='color:white; font-size: 12px; opacity: 75%'>This password will expire in the next 24 hours.</p>
+            <br>
+            <a href='https://codex-bu.appspot.com/'>
+                <img src='https://i.ibb.co/VTB4vfy/logo1.png' style='width: 6rem; height: 6rem;'>
+            </a>
+            <p style='color:white; font-size: 12px; opacity: 100%'>codeX © 2021</p>
+        </center>
     </div>
-
 </div>
 
 </body>
