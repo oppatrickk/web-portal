3f509a7f8a04eda84747f127576ad3a7d24bfb73
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2021-12-18 02:04:55 +0800

    TEst

--- index.php
+++ index.php
@@ -10,18 +10,16 @@ session_start();
 if(!isset($_SESSION["user_login"]) || $_SESSION["user_login"] !== true);
 else{
 
-    /*
     if ($cloud == 1){
         switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
             case '/':
-                require 'pages/dashboard/dashboard.php';
+                header("Location: https://codex-bu.appspot.com/pages/dashboard/dashboard.php");
                 break;
             default:
                 http_response_code(404);
                 exit('Not Found');
         }
-    }*/
-
+    }
 
     header("location: pages/dashboard/dashboard.php");
     exit;
