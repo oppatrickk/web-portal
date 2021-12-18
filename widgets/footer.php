0e405a9fb08024b3531a0426135347602108b232
John Patrick Prieto <70645552+oppatrickk@users.noreply.github.com>
2021-12-18 11:24:51 +0800

    Version 4.1

--- widgets/footer.php
+++ widgets/footer.php
@@ -34,9 +34,6 @@
                 <h5 class="mb-4">About</h5>
                 <ul class="list-unstyled">
                     <li class="mt-2">
-                        <a href="<?php echo $about_path; ?>" style="text-decoration:none; color: dimgray" rel="noopener noreferrer">About codeX</a>
-                    </li>
-                    <li class="mt-2">
                         <a href="<?php echo $founders_path; ?>" style="text-decoration:none; color: dimgray">Founders</a>
                     </li>
                 </ul>
@@ -45,9 +42,6 @@
             <div class="col-md-3 mb-md-0 mb-3">
                 <h5 class="mb-4">Get Help</h5>
                 <ul class="list-unstyled">
-                    <li class="mt-2">
-                        <a href="<?php echo $forum_path; ?>" style="text-decoration:none; color: dimgray" rel="noopener noreferrer">FAQs</a>
-                    </li>
                     <li class="mt-2">
                         <a href="<?php echo $report_path; ?>" style="text-decoration:none; color: dimgray" rel="noopener noreferrer">Report Issue</a>
                     </li>
